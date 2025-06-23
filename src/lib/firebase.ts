cabec6fa0a1d7e5e8786ad894dcbf9ea69c94169
HardzGuardian <sagarsalunkhe98@gmail.com>
2025-06-23 16:49:46 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,21 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Replace this with your own Firebase project configuration.
-//
-// To find your Firebase config object:
-// 1. Go to your project in the Firebase console: https://console.firebase.google.com/
-// 2. In the left-hand menu, click the settings gear icon, then select "Project settings".
-// 3. Under the "General" tab, scroll down to the "Your apps" section.
-// 4. In the "SDK setup and configuration" section, select the "Config" option.
-// 5. Copy the entire 'firebaseConfig' object and paste it here, replacing the placeholder object below.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAd7zlooTOmIdZHTq7SGbC4EaYLTmZ3Fc8",
+  authDomain: "noteverse-giy72.firebaseapp.com",
+  projectId: "noteverse-giy72",
+  storageBucket: "noteverse-giy72.firebasestorage.app",
+  messagingSenderId: "812484800991",
+  appId: "1:812484800991:web:797aa704e1468ccf3f146a"
 };
 
 // Initialize Firebase
