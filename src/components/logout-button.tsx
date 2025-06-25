9d6e0d2d8fb78281f3d2f3464e1a378c2b6c51a6
HardzGuardian <sagarsalunkhe98@gmail.com>
2025-06-25 07:26:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -1,8 +1,27 @@
 "use client";
 
-import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+
+const LogOutIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+    <polyline points="16 17 21 12 16 7" />
+    <line x1="21" x2="9" y1="12" y2="12" />
+  </svg>
+);
+
 
 export function LogoutButton() {
   const router = useRouter();
@@ -16,7 +35,7 @@ export function LogoutButton() {
 
   return (
     <DropdownMenuItem onClick={handleLogout}>
-      <LogOut className="mr-2 h-4 w-4" />
+      <LogOutIcon className="mr-2 h-4 w-4" />
       <span>Logout</span>
     </DropdownMenuItem>
   );
